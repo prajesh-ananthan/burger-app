42d5890c24a5757e7d6f30651135ae071a042e98
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-09-20 22:00:26 +0800

    125 - Starting implementation of Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Auxx from '../../hoc/Auxx'
+import classes from './Layout.css'
+// import './Layout.css'
 
 const layout = (props) => (
 
     <Auxx>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={classes}>
             {props.children}
         </main>
     </Auxx>
