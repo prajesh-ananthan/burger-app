836010c0bc3600a99e507f5a6a01ac3addb71303
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-09-20 22:01:25 +0800

    Fix on deriving Content class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 import Auxx from '../../hoc/Auxx'
 import classes from './Layout.css'
-// import './Layout.css'
 
 const layout = (props) => (
 
     <Auxx>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main className={classes}>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxx>
