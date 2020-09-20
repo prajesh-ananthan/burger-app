2f657749fb7d5260d3f54d5253e6a42282a01257
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-09-20 19:48:05 +0800

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Auxx from '../../hoc/Auxx'
+
+const layout = (props) => (
+
+    <Auxx>
+        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxx>
+
+);
+
+export default layout;
